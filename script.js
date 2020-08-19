7817e82ff120f7b4b7a961ab1d669c2602009ed7
Luis Feliz <Luisfeliz@gmail.com>
2020-08-19 15:02:11 -0400

    chore(PWDGEN) Adding logic to print results to HTML

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var generateBtn = document.querySelector("#generate");
 function start() {
    console.log("Password Generated");
    var password = generatePassword();
-   var passwordText = document.querySelector("#password");
-   passwordText.value = password;
+  //  var passwordText = document.querySelector("#password");
+  //  passwordText.value = password;
 }
 //return a string
 function generatePassword() {
@@ -102,103 +102,70 @@ function generatePassword() {
    // ======= functions definition ======
   function getUserOptions() {
     var isUserReady = confirm("Hello Are you ready for the password process ?");
-    
-
     if (isUserReady) {
       var length = prompt("How many chratcers do you want your password to have ?");
       var passwordLength = parseInt(length);
       userOptionalChars.push(passwordLength);
-   
       var upperCase = confirm("Does your password to include Upper Case Characters ?");
       userOptionalChars.push(upperCase);
-
-     
       var lowerCase = confirm("Does your password to include Lower Case Characters ?");
       userOptionalChars.push(lowerCase);
-
-
       var specialCase = confirm("Does your password to include Special Case Characters ?");
       userOptionalChars.push(specialCase);
-
       var numeric = confirm("Does your password to include Numeric Characters ?");
       userOptionalChars.push(numeric);
-
     }
     if (!isUserReady) {
       alert("Come back when you are ready for a new pass word");
     }
-    else if (upperCase === false || specialCase === false || numeric === false || lowerCase === false) {
-      alert("Password needs at least one requirement");
+    else if (upperCase === false && specialCase === false && numeric === false && lowerCase === false) {
+      alert("Password Generator has to meet one requirement of : Upper, Lower, special or numeric");
     }
-
-  
     return generatePassword(userOptionalChars);
   }
 
-      // get user options and store in vars
-      // check for length (it should be longer than 8 and shorter than 128)
-      // check if lower
-      // check if upper
-      // check if special
-      // check if numeric
-      // check that at least one is true
-        // else - alert user
     
   function generatePassword(userOptions) {
-
- 
     var password = [];
-    
-for (let index = 0; index < userOptions[0]; index++) {
-
-  if (userOptions[1] === true) {
+  for (let index = 0; index < userOptions[0]; index++) {
+    if (userOptions[1] === true) {
       const upperCase = Math.floor(Math.random() * upperCasedCharacters.length);
       password.push(upperCasedCharacters[upperCase]);
       userOptions[0]--;
     }
+   
     if (userOptions[2] === true) {
       const lowerCase = Math.floor(Math.random() * lowerCasedCharacters.length);
       password.push(lowerCasedCharacters[lowerCase]);
       userOptions[0]--;
     }
+    
     if (userOptions[3] === true) {
       const specialCase = Math.floor(Math.random() * specialCharacters.length);
       password.push(specialCharacters[specialCase]);
       userOptions[0]--;  
     }
+    
     if (userOptions[4] === true) {
       const numeric = Math.floor(Math.random() * numericCharacters.length);
       password.push(numericCharacters[numeric]);
       userOptions[0]--;
     }
+    
+    }
 
+    var passwordText = document.querySelector("#password");
+    return (passwordText.outerHTML=password.join(' '))  ; 
 
-      // if lower
-        // push a random lower char to password
-        // add lowerCharsArray to userOptionalChars
-      // if upper
-        // push a random upper char to password
-        // add upperCharsArray to userOptionalChars
-      // if special
-        // push a random special char to password
-      
-        // add specialCharsArray to userOptionalChars
-      // if numeric
-        // push a random numeric char to password
-        // add numericCharsArray to userOptionalChars
-      // for loop between start number of elemnts in password to the requested number of charactars
-      // mutate the array to a string
-      // return password string
-    } 
-    console.log(password.join(' ')); 
   }
    // ======= functions calls (start) ======
     getUserOptions();
   
-   // generatePassword(userOption)
+
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", start);
+
 // var expectSpecialChars = false;
 // var expectNumericChars = false;
 // var expectUppercaseChars = false;
